d625aec480920f10c9bb50079bcb9c94fc597373
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-30 15:58:33 -0500

    feat: Added stats endpoint

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,23 @@
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
 def ret_status():
     return(jsonify(status="OK"))
+
+@app_views.route('/stats', strict_slashes=False)
+def count_clases():
+    classes = [Amenity, City, Place, Review, State, User]
+    ret_dict = dict()
+    for cls in classes:
+        ret_dict[cls.__name__] = storage.count(cls)
+    return(jsonify(ret_dict))
