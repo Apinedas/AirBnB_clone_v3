89856a940a17b2524c692a112b3d4d9ed66e9539
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-31 23:35:57 -0500

    feat: places_amenities

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -12,12 +12,50 @@ from models.place import Place
 @app_views.route(
     'places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
 def places_amenities(place_id):
-
     place = storage.get(Place, place_id)
     if not place:
         abort(404)
-
     if storage_t == 'db':
-        return jsonify(place.amenities)
+        return jsonify([amenity.to_dict() for amenity in place.amenities])
     else:
         return jsonify(place.amenity_ids)
+
+
+@app_views.route('places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def delete_amenity(place_id, amenity_id):
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if not place or not amenity:
+        abort(404)
+    if storage_t == 'db':
+        if amenity not in place.amenities:
+            abort(404)
+        place.amenities.remove(amenity)
+    else:
+        if amenity.id not in place.amenity_ids:
+            abort(404)
+        place.amenity_ids.remove(amenity.id)
+    storage.save()
+    return jsonify(), 200
+
+
+@app_views.route('places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'], strict_slashes=False)
+def amenity_to_place(place_id, amenity_id):
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if not place or not amenity:
+        abort(404)
+
+    if storage_t == 'db':
+        if amenity in place.amenities:
+            return jsonify(amenity.to_dict()), 200
+        place.amenities.append(amenity)
+    else:
+        if amenity.id in place.amenity_ids:
+            return jsonify(amenity.to_dict()), 200
+        place.amenity_ids.append(amenity.id)
+
+    storage.save()
+    return jsonify(amenity.to_dict()), 201
